6273666f8000d4049618ac67b14635c01ae44eb9
dugonzal <ciclcontinuo2@gmail.com>
2023-04-21 01:34:55 +0000

    sin norma juju, ns si es bueno o malo xd

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 00:44:34 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/04/21 01:30:11 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/04/21 01:34:08 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,14 @@ char *Megaphone(char *str)
 	return (str);
 }
 
-int main(int Ac, char **Av)
-{
-  int x;
-
+int main(int Ac, char **Av) {
   if (Ac == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
   }
-  x = 0;
-  if (Av[x][0] == '\0')
+  if (Av[1][0] == '\0')
 	   std::cout << "c++ > 2 " << std::endl; 
-  while (++x < Ac) {
+  for (int x = 1; x < Ac; x++) {
 	  Megaphone(Av[x]);
   }
   std::cout << std::endl; // salto de linea
