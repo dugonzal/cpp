3b545412a4b93999432696bc4dd71ba0a218df44
dugonzal <ciclcontinuo2@gmail.com>
2023-04-26 11:37:24 +0000

    update 00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,35 +6,22 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 00:44:34 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/04/21 13:33:27 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/04/26 11:36:54 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-char *megaPhone(char *phone) {
-	if (!phone)
-		return (NULL);
-	
-	for (int i = -1; phone[i]; i++) // recorro la cadena
-	  if (phone[i] >= 'a' && phone[i] <= 'z') // solo hay dos, asi 'a' - 32 = 'A' 
-		std::cout << (phone[i] = phone[i] - 32); // asccii #
-	  else
-		std::cout << phone[i];
-	
-	return (phone);
-}
 
 int main(int ac, char **av, char **) {
-  if (ac == 1) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-  }
-  for (int x = 1; x < ac; x++) // salto el nombre del programa y recorro los argumentos
-	  if (!megaPhone(av[x]))
-		continue;  
-
-  std::cout << std::endl; // salto de linea
-  // lolo -->>execve("/bin/fsanitize", av, env); // muestro leaks
+	if (ac > 1) {
+		for (int i = 1; i < ac; i++)
+		  for (int j = 0; av[i][j]; j++)
+			 std::cout << (char)std::toupper(av[i][j]);
+	}
+	else
+	  std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	std::cout << std::endl; // salto de linea
+	// lolo -->>execve("/bin/fsanitize", av, env); // muestro leaks
   return (0);
 }
