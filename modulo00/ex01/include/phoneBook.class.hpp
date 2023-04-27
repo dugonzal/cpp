9e4951f612c488b7b76128dbc607af0415a052a8
dugonzal <ciclcontinuo2@gmail.com>
2023-04-27 21:13:48 +0000

    agrego un poco de color, muy casual

--- modulo00/ex01/include/phoneBook.class.hpp
+++ modulo00/ex01/include/phoneBook.class.hpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 18:36:49 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/04/27 20:48:05 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/04/27 21:09:28 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 
 # include <iostream>
 #include <stdlib.h>
+
+#define BLUE  "\033[34m"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
 class phoneBook {
   public:
   phoneBook (void); // constructor 
--- modulo00/ex01/src/phoneBook.class.cpp
+++ modulo00/ex01/src/phoneBook.class.cpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 19:32:23 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/04/27 20:53:30 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/04/27 21:13:10 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,22 @@ phoneBook::~phoneBook (void) { //desctructor de la clase
 
 void phoneBook::Book(void) {
   std::string option;
-  std::cout << "----------------------------------" << std::endl;
-  std::cout << "|     phoneBook que empiece      |" << std::endl;
-  std::cout << "|         la divercion           |" << std::endl;
-  std::cout << "|       tus opciones son:        |" << std::endl;
-  std::cout << "| 1. ADD   2. SEARCH   3. EXIT   |" << std::endl;
-  std::cout << "|        By: Dugonzal            |" << std::endl;
-  std::cout << "|--------------------------------|" << std::endl;
+  
+  std::cout << GREEN << "----------------------------------"  << RESET << std::endl;
+  std::cout << GREEN << "|     phoneBook que empiece       |" << RESET << std::endl;
+  std::cout << GREEN << "|         la diversion            |" << RESET << std::endl;
+  std::cout << GREEN << "|       tus opciones son:         |" << RESET << std::endl;
+  std::cout << GREEN << "| 1. ADD   2. SEARCH   3. EXIT    |" << RESET << std::endl;
+  std::cout << GREEN << "|        By: Dugonzal             |" << RESET << std::endl;
+  std::cout << GREEN << "----------------------------------"  << RESET << std::endl;
   while (42)
   {
 	std::cout << "Introduce una opcion: ";
 	std::cin >> option;
 	if (option == "ADD" || option == "1")
-	  std::cout << "ADD" << std::endl;
+	  std::cout << BLUE << "ADD" << RESET << std::endl;
 	else if (option == "SEARCH" || option == "2")
-	  std::cout << "SEARCH" << std::endl;
+	  std::cout << GREEN << "SEARCH" << RESET << std::endl;
 	else if (option == "EXIT" || option == "3")
 	{
 	  std::cout << "EXIT" << std::endl;
