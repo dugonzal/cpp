de0c70276ba5a54993d537c39dd251e0b2c2a35a
dugonzal <ciclcontinuo2@gmail.com>
2023-04-27 18:49:57 +0000

    phonebook

---
+++ modulo00/ex01/include/phoneBook.class.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 18:36:49 by Dugonzal          #+#    #+#             */
+/*   Updated: 2023/04/27 18:37:05 by Dugonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_HPP
+# define PHONEBOOK_CLASS_HPP
+
+# include <iostream>
+
+#endif
---
+++ modulo00/ex01/src/main.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 18:30:26 by Dugonzal          #+#    #+#             */
+/*   Updated: 2023/04/27 18:46:18 by Dugonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/phoneBook.class.hpp"
+
+int main(void)
+{
+  std::cout << "Bienvenido a la agenda de contactos" << std::endl;
+  return (0);
+}
