b6bb230bd91a0237da6e69f9f2ee1a2429b46126
dugonzal <ciclocontinuo2@gmail.com>
2023-09-24 00:02:01 +0200

    creo el makfile

---
+++ 04/ex00/inc/Animal.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/23 23:56:15 by Dugonzal          #+#    #+#             */
+/*   Updated: 2023/09/23 23:58:13 by Dugonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+using std::cout; 
+using std::endl;
+using std::string;
+
+class Animal <%
+	private:
+		string type;
+	public:
+
+%>;
+
+#endif // ANIMAL_HPP
