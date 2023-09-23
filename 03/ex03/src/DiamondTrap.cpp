c0b66c0c0b94dfd054ba8fae57a707bfdb73c66c
dugonzal <ciclocontinuo2@gmail.com>
2023-09-23 10:56:13 +0200

    da igual a que clase de la heredadas llame para establecer los valores, se establecen en la clase base

--- 03/ex03/src/DiamondTrap.cpp
+++ 03/ex03/src/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 21:44:38 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/09/23 10:46:21 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/09/23 10:55:34 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 // si o si tengo que pasarle un arreglo vacio
 DiamondTrap::DiamondTrap( void ): FragTrap(""), ScavTrap("") <%
 	cout << "constructor DiamondTrap" << endl;
-	FragTrap::setHitPoint(FragTrap::getHitPoint());
+	FragTrap::setHitPoint( FragTrap::getHitPoint() );
+	ScavTrap::setHitPoint(ScavTrap::getHitPoint());
 	cout << FragTrap::getHitPoint() << endl;
+	cout << ScavTrap::getEnergyPoint() << endl;
 %>
 
 DiamondTrap::~DiamondTrap( void )<%
@@ -26,12 +28,17 @@ DiamondTrap::~DiamondTrap( void )<%
 DiamondTrap::DiamondTrap( const DiamondTrap &other ): FragTrap(other), ScavTrap(other) <%
 	cout << "constructor DiamondTrap with copy" << endl;
 	FragTrap::setHitPoint(FragTrap::getHitPoint());
+	ScavTrap::setHitPoint(ScavTrap::getEnergyPoint());
 	*this = other;
 %>
 
 DiamondTrap::DiamondTrap( const string & n ): FragTrap(n + "_clap_name"), ScavTrap(n + "_clap_name"), _name(n + "_clap_name") <%
 	cout << "constructor DiamondTrap with name" << endl;
 	FragTrap::setHitPoint(FragTrap::getHitPoint());
+	ScavTrap::setHitPoint(ScavTrap::getHitPoint());
+	/*
+	 llamando a la derivada, se establece en la derivada de la derivada es alucinante
+	*/
 %>
 
 const DiamondTrap &DiamondTrap::operator=( const DiamondTrap &other )<%
