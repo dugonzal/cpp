b8103f23355412d843f06f3de54ffe1b5e2266e8
dugonzal <ciclocontinuo2@gmail.com>
2023-09-24 11:38:21 +0200

    agrego la clase gato derivada de animal

--- 04/ex00/inc/Cat.hpp
+++ 04/ex00/inc/Cat.hpp
@@ -1,31 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/24 10:05:53 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/09/24 10:20:21 by Dugonzal         ###   ########.fr       */
+/*   Created: 2023/09/24 11:35:27 by Dugonzal          #+#    #+#             */
+/*   Updated: 2023/09/24 11:37:59 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/Dog.hpp"
+#ifndef CAT_HPP
+# define CAT_HPP
 
-Dog::Dog( void ): Animal("Dog") <%
-	cout << "constructor Dog" << endl;
-%>
+#include "Animal.hpp"
 
-Dog::~Dog( void )<%
-	cout << "destructor Dog" << endl;
-%>
+class Cat: public Animal <%
+	public:
+		Cat( void );
+		~Cat( void );
+		Cat( const Cat &other );
+		operator=( const Cat &other);
 
-Dog::Dog( const Dog &other ): Animal(other) <%
-	*this = other;
-%>
+%>;
 
-const Dog &Dog::operator=( const Dog &other)<%
-	// me gusta cpp, pero es demasiado abstracto
-	Animal::operator=(other);
-	return (*this);
-%>
+
+# endif
