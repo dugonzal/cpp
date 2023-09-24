f51f3a2d6511e7c812eac0ef8c547516998748b5
dugonzal <ciclocontinuo2@gmail.com>
2023-09-24 13:13:52 +0200

    soy poliforme, toda la vida lo senti, pero ahora empiezo a ser lo que soy

--- 04/ex00/src/main.cpp
+++ 04/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 23:55:27 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/09/24 12:37:29 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/09/24 13:13:09 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int main() <%
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-	
+
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	std::cout << meta->getType() << " " << std::endl;
-
+	  
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
