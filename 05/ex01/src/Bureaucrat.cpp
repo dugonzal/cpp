810c0c0f722a478bdd6a2fb2ade6c9c552251667
dugonzal <ciclocontinuo2@gmail.com>
2023-11-08 19:43:28 +0100

    se ve mejor asi signForm, creo que lo voy a dejar asi

--- 05/ex01/src/Bureaucrat.cpp
+++ 05/ex01/src/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 18:16:28 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/08 19:14:35 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/08 19:42:57 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,9 @@ void Bureaucrat::increment(void) <%
 
 void Bureaucrat::signForm(const Form &other) const<%
 	  if (grade <= other.getGrade())
-		std::cout << *this << " signed " << other;
+		std::cout << name << " signed " << other.getName() << std::endl;
 	  else 
-		std::cout << *this << " couldn’t sign " << other;
+		std::cout << name << " couldn’t sign " << other.getName() << std::endl;
 %>
 
 std::ostream &operator<<(std::ostream &os, const Bureaucrat &other) <%
