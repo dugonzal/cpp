341b0259c2d03f893e602e438a68d4bc5b9b9f31
dugonzal <ciclocontinuo2@gmail.com>
2023-11-14 20:09:54 +0100

    [^_^]

--- 06/ex00/src/main.cpp
+++ 06/ex00/src/main.cpp
@@ -6,16 +6,20 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 14:07:35 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/14 14:29:17 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/14 14:38:58 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 
+/*Este literal será uno de los siguientes tipos escalares: char, int, float o double. Solo se utilizará la notación
+decimal.*/
 int	main(int ac, const char **av) <%
 
 	if (ac != 2)
 	  return (0);
-	std::cout << av[1] << std::endl;
+
+	std::string str(av[1]);
+	std::cout << str << std::endl;
 	return (0);
 %>
