8340bec5e0fc7454470af8884e7b5b2e2e98eca8
dugonzal <ciclocontinuo2@gmail.com>
2023-11-16 12:03:39 +0100

    este concepto de serializacion de datos me ha gustado bastante indagare mas sobre esto

--- 06/ex01/src/main.cpp
+++ 06/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 08:24:38 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/16 11:38:10 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/16 12:02:28 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ int	main(void) <%
 
 	Data a;
 
-	a.name = "hola";
-	a.lastName = "per";
+	a.name = "camilo";
+	a.lastName = "tejeron";
 	a.age = 23;
 	
 	uintptr_t  tmp = serialize(&a);
 	
-	std::cout << "datos serializados  " << tmp <<  std::endl;
+	std::cout << "datos serializados en hexadecimal " << std::hex << tmp << std::dec <<  std::endl;
 	
 	Data *b ;
 
 	b = deserialize(tmp);
 
 	std::cout << b->name << std::endl;
+	std::cout << b->lastName << std::endl;
+	std::cout << b->age << std::endl;
 //	delete b;
 //	delete a;
 	return (0);
