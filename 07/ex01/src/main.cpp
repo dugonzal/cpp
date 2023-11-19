788de3bb6f9aee78f0b780f4397818dd39724f66
dugonzal <ciclocontinuo2@gmail.com>
2023-11-19 19:01:32 +0100

    ok, ahora si lo voy pillando

--- 07/ex01/src/main.cpp
+++ 07/ex01/src/main.cpp
@@ -6,22 +6,26 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 16:50:02 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/19 18:48:44 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/19 19:00:31 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/iter.hpp" 
 
-void print(const std::string &s) <%
-	std::cout << s << std::endl;
+template <class p>
+void print(p x) <%
+	std::cout << x << std::endl;
 %>
+
 
 int	main(void) <%
 
-	std::string a[2] = {"gola mi", "ppppppppp ppp "};
-
-	::iter<std::string>(a, (sizeof(a) / sizeof(a[0])), print);
-	::iter(a, (sizeof(a) / sizeof(a[0])), print);
+	std::string a[] = {"cadena 1", "cadena 2", "cadena 3"};
+	int arrInt[] = {2334, 4343, 4343, 4343, 4343, 4343 ,43};
+	 
+	::iter<std::string>(a, 2, ::print<std::string>);
+	std::cout << std::endl << std::endl;
+	::iter(arrInt, (sizeof(arrInt) / sizeof(arrInt[0])), print<int>);
 
 	return (0);
 %>
