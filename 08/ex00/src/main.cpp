e862c9712729b3485e2845781b7a9c767909ea99
dugonzal <ciclocontinuo2@gmail.com>
2023-11-23 17:36:28 +0100

    afino sin, mas

--- 08/ex00/src/main.cpp
+++ 08/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 11:49:27 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/23 17:28:35 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/23 17:36:06 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,25 @@ void printVector(std::vector<int> tmp) <%
 	
 	std::cout << std::endl;
 	
-	//for (std::vector<std::string>::reverse_iterator it = tmp.rbegin(); it != tmp.rend(); it++)
-	//	std::cout << *it << std::endl;
+	for (std::vector<int>::reverse_iterator it = tmp.rbegin(); it != tmp.rend(); it++)
+		std::cout << *it << std::endl;
 %>
 
 
 int	main(int ac, char **av) <%
 	int array[] = {2, 3, 4, 5, 1, 23, 32};
 	
-	if (ac != 2)
-	  return (1);
-    std::vector<int> arrayInt(array, array + sizeof(array) / sizeof(array[0]));
-	
-	std::list<int> arrayList(array, array + sizeof(array) / sizeof(array[0]));
-	
-	std::deque<int> arrayDuque(array, array + sizeof(array) / sizeof(array[0]));
-	
-	(void)av;
-	
-	printVector((arrayInt));
 	try <%
-		// literalmente dice que tiene que ser el segundo parametro un enetero asi que f, pero mas facil	
+		if (ac != 2)
+			return (1);
+		std::vector<int> arrayInt(array, array + sizeof(array) / sizeof(array[0]));
+	
+		std::list<int> arrayList(array, array + sizeof(array) / sizeof(array[0]));
+	
+		std::deque<int> arrayDuque(array, array + sizeof(array) / sizeof(array[0]));
+	
+		printVector((arrayInt));
+	
 		// vector
 		::easyfind< std::vector<int> >(arrayInt, std::atoi(av[1]));
 		easyfind(arrayInt, std::atoi(av[1]));
@@ -49,12 +47,12 @@ int	main(int ac, char **av) <%
 		easyfind(arrayList, std::atoi(av[1]));
 
 		// deque 
-		::easyfind< std::deque<int>  >(arrayDuque, std::atoi(av[1]));
+		::easyfind< std::deque<int> >(arrayDuque, std::atoi(av[1]));
 		easyfind(arrayDuque, std::atoi(av[1]));
 		
 	%>
 	catch(std::exception &e) <%
-		std::cerr << e.what() << std::endl;
+		std::cerr << "Error: " << e.what() << std::endl;
 	%>
-
+	return (0);
 %>
