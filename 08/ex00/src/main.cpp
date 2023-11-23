1b1e036b498eea76d841515ae7b1a3bcbc7fb8e3
dugonzal <ciclocontinuo2@gmail.com>
2023-11-23 13:19:44 +0100

    mola los metodos, bastante simples

--- 08/ex00/src/main.cpp
+++ 08/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 11:49:27 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/23 13:10:36 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/23 13:19:26 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,14 @@ int	main(void) <%
 	
     std::vector<int> tmp(array, array + sizeof(array) / sizeof(array[0]));
 
-	std::cout << tmp.back() << std::endl;
-	std::cout << tmp.front() << std::endl;
+	std::cout << tmp.back() << std::endl; // al  final del vector 
+	std::cout << tmp.front() << std::endl; // al inicio del vector
+	std::cout << tmp.at(4) << std::endl; // con at se accede a un elemnto concreto del vector
+	std::cout << tmp.size() << std::endl; 
+	std::cout << tmp.capacity() << std::endl; 
+	tmp.resize(0) ; 
+	tmp.swap(0) ; 
+	std::cout << tmp.capacity() << std::endl; 
 //	::easyfind<int>(a, 3);
 
 %>
