3a658a06aca84b6d7bc433ba755a0688a6324e0e
dugonzal <ciclocontinuo2@gmail.com>
2023-11-23 13:52:52 +0100

    iterato de vector viceversa

--- 08/ex00/src/main.cpp
+++ 08/ex00/src/main.cpp
@@ -6,25 +6,27 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 11:49:27 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/23 13:19:26 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/23 13:40:48 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../inc/easy.hpp"
 
 int	main(void) <%
-	int array[] = {2, 3, 4, 5, 1, 23, 32};
+	//char array[] = {2, 3, 4, 5, 1, 23, 32};
+    //std::vector<int> tmp(array, array + sizeof(array) / sizeof(array[0]));
 	
-    std::vector<int> tmp(array, array + sizeof(array) / sizeof(array[0]));
+	std::string str[] = {"hola", "mundo", "vectorial", "compon", "clases"};	
 
-	std::cout << tmp.back() << std::endl; // al  final del vector 
-	std::cout << tmp.front() << std::endl; // al inicio del vector
-	std::cout << tmp.at(4) << std::endl; // con at se accede a un elemnto concreto del vector
-	std::cout << tmp.size() << std::endl; 
-	std::cout << tmp.capacity() << std::endl; 
-	tmp.resize(0) ; 
-	tmp.swap(0) ; 
-	std::cout << tmp.capacity() << std::endl; 
-//	::easyfind<int>(a, 3);
+    std::vector<std::string> tmp(str, str + sizeof(str) / sizeof(str[0]));
+
+	tmp.push_back("end");
+	for(std::vector<std::string>::iterator it = tmp.begin(); it != tmp.end(); it++)
+		std::cout << *it << std::endl;
+	std::cout << std::endl;
+	for (std::vector<std::string>::reverse_iterator it = tmp.rbegin(); it != tmp.rend(); it++)
+		std::cout << *it << std::endl;
+
+	//::easyfind<int>(tmp, 3);
 
 %>
