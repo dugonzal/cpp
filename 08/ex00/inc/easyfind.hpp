156f9b121667702eb136981f3270c3f18f366a20
dugonzal <ciclocontinuo2@gmail.com>
2023-11-27 23:25:58 +0100

    update

--- 08/ex00/inc/easyfind.hpp
+++ 08/ex00/inc/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 12:28:02 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/27 17:15:43 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/27 19:02:48 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,10 @@
 template <typename T>
 void easyfind(const T &container, const int &value) <%
 
-	class T::const_iterator it = std::find(container.begin(), container.end(), value);
-	if (it != container.end()) <%
-	  std::cout << "value found in container: " << std::endl;
-	  return ;
-	%>
-	//std::cout << typeid(T).name() << std::endl;
-	throw std::out_of_range("value no found  in container");
-	//throw std::runtime_error("value no found  in container");
+	typename T::const_iterator it = std::find(container.begin(), container.end(), value);
+	if (it != container.end())
+		std::cout << "value found in container: " << std::endl;
+	else
+		throw std::out_of_range("value no found  in container");
 %>
 
