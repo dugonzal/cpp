84666fdffb3bab6a1c90abee1f87310a2999acff
dugonzal <ciclocontinuo2@gmail.com>
2023-11-27 18:41:09 +0100

    no se que configuracion mole mas xd

--- 08/ex01/inc/Span.hpp
+++ 08/ex01/inc/Span.hpp
@@ -6,27 +6,27 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 17:50:34 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/27 16:08:31 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/27 18:30:44 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # pragma once
 
 # include <iostream>
-# include <vector>
 # include <cstdlib>
 # include <ctime>
+# include <vector>
 # include <list>
 # include <deque>
 # include <stdexcept>
-
+# include <algorithm>
 //template < class T >
 template < class T = class TT >
 
 class Span <%
 	
 	private:
-		mutable T					array;
+		mutable std::vector<T>			array;
 		mutable unsigned int			n;
 	
 	public:
@@ -36,6 +36,6 @@ class Span <%
 		Span<T>(const Span<T> &other);
 		Span<T> &operator=(const Span<T> &other);
 		void	addNumber(unsigned int o) const ;
-		void	addNumber(const T &other);
+		//void	addNumber(const T &other) ;
 		void	show(void);
 %>;
--- 08/ex01/src/Span.cpp
+++ 08/ex01/src/Span.cpp
@@ -6,17 +6,17 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 17:52:49 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/27 16:12:58 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/27 18:33:45 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../inc/Span.hpp"
 
 template <class T>
-Span<T>::Span(void): n(0) <% %>
+Span<T>::Span(void): n(0), array(array.reserve(0)) <% %>
 
 template <class T>
-Span<T>::Span(unsigned int _n): n(_n) <% %>
+Span<T>::Span(unsigned int _n): n(_n), array(array.reserve(_n)) <% %>
 	  
 template <class T>
 Span<T>::~Span(void) <% %>
@@ -36,20 +36,26 @@ Span<T> &Span<T>::operator=(const Span<T> &other) <%
 	
 template <class T>
 void	Span<T>::addNumber(unsigned int o) const<%
-	if (this->array.size() >= n)
+	if (array.size() == n)
 		  throw (std::runtime_error("out range, bye"));
 	array.push_back(o); 
 %>	
 
+/*
 template <class T>
 void	Span<T>::addNumber(const T &other) <%
-	  array.insert(array.end(), other.begin(), other.end());
+	
+	for(class T::iterator it = other.begin(); it != other.end(); it++) <%
+		  if (array.size() == n)
+			  throw (std::runtime_error("out range, bye"));
+			addNumber(*it);
+	%>
 %>
 
+*/ 
 template <class T>
 void	Span<T>::show(void) <%
 
-	for (class T::iterator it = array.begin(); it != array.end(); it++)
+	for (class std::vector<T>::iterator it = array.begin(); it != array.end(); it++)
 		std::cout << *it << std::endl;
 %>
-		
