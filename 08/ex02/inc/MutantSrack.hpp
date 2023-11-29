c90d965bc612f11a3be2ffb39ce48cffa732db74
dugonzal <ciclocontinuo2@gmail.com>
2023-11-29 15:22:59 +0100

    lo empiezo a ver, pero no veo como definirlo

--- 08/ex02/inc/MutantSrack.hpp
+++ 08/ex02/inc/MutantSrack.hpp
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 12:45:07 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/11/28 12:49:56 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/11/29 15:22:24 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,16 @@
 # include <iostream>
 # include <stack>
 
-template <class T>
+// aqui esta el truco que stack, usa el contenedor deque, es por hay
+//template<class T, class Container = deque<T> >
+template <class T >
 class	MutantStack: public std::stack<T> <%
 	  private:
 
-
 	  public:
+		  MutantStack<T>(void) <% %>
 
+		  ~MutantStack<T>(void) <% %>
+		  
+		  MutantStack<T>(const MutantStack<T> &other) <% *this = other; %>
 %>;
