4533d0e9429f7b8a88f7d06a92041eb5a250cb33
dugonzal <ciclocontinuo2@gmail.com>
2023-12-02 23:08:35 +0100

    struct proyect and Makefile is going on btc

---
+++ 09/ex00/inc/BitcoinExchange.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/02 23:06:34 by Dugonzal          #+#    #+#             */
+/*   Updated: 2023/12/02 23:07:44 by Dugonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# pragma once
+
+# include <iostream>
+
+# include <map>
+
+class BitcoinExchange <%
+	private:
+
+
+	public:
+
+
+%>;
